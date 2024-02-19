f06f7ace3ac8e99daf4656d3061d07a2353521c3
= <=>
2024-02-18 23:31:40 -0300

    refactor: adjust installments and invoices tables

--- database/migrations/2023_11_16_145816_create_invoices_table.php
+++ database/migrations/2023_11_16_145816_create_invoices_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Card::class)->onDelete('cascade');
-            $table->double('invoice_value');
+            // $table->double('invoice_value');
             $table->date('invoice_expiration');
             $table->date('invoice_closure');
             // $table->timestamps();
