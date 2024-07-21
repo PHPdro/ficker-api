6a95dc0e763a5f0da777a940f9aa517f647aa21c
PHPdro <pedro110302@gmail.com>
2024-07-21 15:27:17 -0300

    docker: update dockerfile

--- database/migrations/2023_08_04_201942_create_categories_table.php
+++ database/migrations/2023_08_04_201942_create_categories_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->onDelete('cascade');
+            $table->foreignIdFor(User::class)->cascadeOnDelete();
             $table->string('category_description');
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
